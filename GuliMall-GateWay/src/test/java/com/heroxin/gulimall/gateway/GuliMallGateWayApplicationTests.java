fd6600d49902ae27c0edcde87f80e6cbcd28cf95
heroxin <hero_xin59@163.com>
2023-03-29 17:53:12 +0800

    GateWay

---
+++ GuliMall-GateWay/src/test/java/com/heroxin/gulimall/gateway/GuliMallGateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heroxin.gulimall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliMallGateWayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
