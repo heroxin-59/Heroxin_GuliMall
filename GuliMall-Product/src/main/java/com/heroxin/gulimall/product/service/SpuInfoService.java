e1f4c205499be55dd6eb41fd615f273a3ac2a5f5
Heroxin <hero_xin59@163.com>
2023-04-10 13:44:53 +0800

    商品维护---spu查询

--- GuliMall-Product/src/main/java/com/heroxin/gulimall/product/service/SpuInfoService.java
+++ GuliMall-Product/src/main/java/com/heroxin/gulimall/product/service/SpuInfoService.java
@@ -21,5 +21,7 @@ public interface SpuInfoService extends IService<SpuInfoEntity> {
     void saveSpuInfo(SpuSaveVo spuInfo);
 
     void saveBaseSpuInfo(SpuInfoEntity spuInfoEntity);
+
+    PageUtils queryPageByCondition(Map<String, Object> params);
 }
 
--- GuliMall-Product/src/main/java/com/heroxin/gulimall/product/service/impl/SkuInfoServiceImpl.java
+++ GuliMall-Product/src/main/java/com/heroxin/gulimall/product/service/impl/SkuInfoServiceImpl.java
@@ -1,7 +1,10 @@
 package com.heroxin.gulimall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -11,6 +14,7 @@ import com.heroxin.gulimall.common.utils.Query;
 import com.heroxin.gulimall.product.dao.SkuInfoDao;
 import com.heroxin.gulimall.product.entity.SkuInfoEntity;
 import com.heroxin.gulimall.product.service.SkuInfoService;
+import org.springframework.util.StringUtils;
 
 
 @Service("skuInfoService")
@@ -29,6 +33,59 @@ public class SkuInfoServiceImpl extends ServiceImpl<SkuInfoDao, SkuInfoEntity> i
     @Override
     public void saveSkuInfo(SkuInfoEntity skuInfoEntity) {
         this.baseMapper.insert(skuInfoEntity);
+    }
+
+    @Override
+    public PageUtils queryPageByCondition(Map<String, Object> params) {
+        QueryWrapper<SkuInfoEntity> queryWrapper = new QueryWrapper<>();
+
+        String key = (String) params.get("key");
+        if (!StringUtils.isEmpty(key) && !"0".equalsIgnoreCase(key)) {
+            queryWrapper.and((wrapper) -> {
+                wrapper.eq("sku_id", key).or().like("sku_name", key);
+            });
+        }
+
+        String catelogId = (String) params.get("catelogId");
+        if (!StringUtils.isEmpty(catelogId) && !"0".equalsIgnoreCase(catelogId)) {
+            queryWrapper.eq("catalog_id", catelogId);
+        }
+
+        String brandId = (String) params.get("brandId");
+        if (!StringUtils.isEmpty(brandId) && !"0".equalsIgnoreCase(brandId)) {
+            queryWrapper.eq("brand_id", brandId);
+        }
+
+        String min = (String) params.get("min");
+        if (!StringUtils.isEmpty(min)) {
+            queryWrapper.ge("price", min);
+        }
+
+        String max = (String) params.get("max");
+
+        if (!StringUtils.isEmpty(max)) {
+            try {
+                BigDecimal bigDecimal = new BigDecimal(max);
+                if (bigDecimal.compareTo(BigDecimal.ZERO) == 1) {
+                    queryWrapper.le("price", max);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        // key:
+        // catelogId: 225
+        // brandId: 9
+        // min: 0
+        // max: 0
+
+        IPage<SkuInfoEntity> page = this.page(
+                new Query<SkuInfoEntity>().getPage(params),
+                queryWrapper
+        );
+
+        return new PageUtils(page);
     }
 
 }
